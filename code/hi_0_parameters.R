9c208212abb82ffb44a3e95046f2d18b1256bea7
Fabio Morea <fabio.morea@areasciencepark.it>
2024-09-21 15:55:10 +0200

    improved management of case studies

--- code/hi_0_parameters.R
+++ code/hi_0_parameters.R
@@ -7,25 +7,59 @@ download_HEUROPE <- F
 folder_H2020 <- "./data/H_2020_csv/"
 folder_HEurope <- "./data/H_EU_csv/"
 
-#network centrality measures and communities
-miny <- 2015 
-maxy <- 2029
 
-# community detection
-community_detection_method <- "WT"
-param = 5 #steps
+case_studies <- list(
+    "hydrogen" = list(
+        selected_esv_topic = "hydrogen energy",
+        destination_path = "./data/filtered-h/",
+        visual_title = "subset topic = ''hydrogen energy'",
+        miny = 2015, 
+        maxy = 2029,
+        community_detection_method = "WT",
+        param = 5 #steps
+    ),
+    "microscopy" = list( 
+        selected_esv_topic = "electron microscopy",
+        destination_path = "./data/filtered-m/",
+        visual_title = "subset topic = 'electron microscopy'",
+        miny = 2015, 
+        maxy = 2029,
+        community_detection_method = "WT",
+        param = 5 #steps
+    ),
+    "pandemics" = list( 
+        selected_esv_topic = "pandemics",
+        destination_path = "./data/filtered-p/",
+        visual_title = "subset topic = 'pandemics'",
+        miny = 2015, 
+        maxy = 2029,
+        community_detection_method = "WT",
+        param = 5 #steps
+    )
+    
+)
 
-# #filter 
-# selected_esv_topic <- "electron microscopy"
-# destination_path <- "./data/filtered-m/"
-# visual_title <- "subset topic = 'electron microscopy'"
 
-selected_esv_topic <- "hydrogen energy"
-destination_path <- "./data/filtered-h/"
-visual_title <- "subset topic = 'hydrogen energy'"
+select_case_study <- function(case_name) {
+    if (!case_name %in% names(case_studies)) {
+        stop("Invalid case study name. Choose from: ", paste(names(case_studies), collapse = ", "))
+    }
+    return(case_studies[[case_name]])
+}
 
+
+############# SELECT THE APPROPRIATE CASE STUDY ##################
+
+ 
+case <- select_case_study("hydrogen")  
+
+
+# apply to variables that are used in other parts of the code
 # 
-# #https://cordis.europa.eu/project/id/101137192
-# selected_esv_topic <- "pandemics"
-# destination_path <- "./data/filtered-p/"
-# visual_title <- "subset topic = 'pandemics'"
+selected_esv_topic <- case$selected_esv_topic
+destination_path <- case$destination_path
+visual_title <- case$visual_title
+miny <-  case$miny
+maxy <- case$maxy
+community_detection_method <- case$community_detection_method 
+param = case$param
